ccea7099c46c7b9fc9d37a7327e8377eab1727a1
akora <akoranteng@msn.com>
2026-01-14 18:36:14 -0500

    Add EF Core setup with Product model, DbContext, factory, and InitialCreate migration

---
+++ BlazoWebAppDemo/BlazoWebAppDemo/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using BlazoWebAppDemo.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+//using BlazoWebAppDemo.Data;
+
+
+namespace BlazorWebApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ BlazoWebAppDemo/BlazoWebAppDemo/Data/AppDbContextFactory.cs
@@ -0,0 +1,24 @@
+﻿using BlazorWebApp.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace BlazoWebAppDemo.Data
+{
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            optionsBuilder.UseSqlite(config.GetConnectionString("DefaultConnection"));
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
